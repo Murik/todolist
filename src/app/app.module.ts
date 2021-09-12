edb2b3e71d6644d1719db721623f6032df474f75
Murik <MTemirov@lanit.ru>
2021-09-12 21:37:02 +0300

    first task view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CategoriesComponent } from './view/categories/categories.component';
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from './view/categories/categories.component';
+import {TaskComponent} from './view/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
+import {Priority} from "../model/Priority";
+import {Task} from "../model/Task";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,13 @@ export class DataHandlerService {
 
   getCategories(): Category[]{
    return TestData.categories;
+  }
+
+  getPriorities(): Priority[]{
+    return TestData.priorities;
+  }
+
+  getTasks(): Task[]{
+    return TestData.tasks;
   }
 }
